bec3a96def29298bd3b5732cfdabaab9b3704495
mj-song00 <niveus128@gmail.com>
2022-09-02 17:57:31 +0900

    2022.09.02_주석추가

--- 1181.js
+++ 1181.js
@@ -11,6 +11,6 @@ input.shift()
 //     arr.sort((a,b) => a -b )
 //     arr.sort((a,b) => a.length -b.length || a.localeCompare(b))
 // }
-const sorted = input.sort((a, b) => a.length - b.length || a.localeCompare(b));
+const sorted = input.sort((a, b) => a.length - b.length || a.localeCompare(b));//사전배열
 const array = new Set(sorted)
-console.log(Array.from(array).join('\n'))
+console.log(Array.from(array).join('\n')) //유사 배열객체나 반복 가능한 객체를 얕게 복사해 새로운 array객채 생성
