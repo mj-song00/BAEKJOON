ac67e331323df16449916b31a578a0f9013a3bac
mj-song00 <niveus128@gmail.com>
2022-09-21 20:47:44 +0900

    2022.09.21

--- 2751.js
+++ 2751.js
@@ -1,10 +1,12 @@
-const fs = require("fs");
-let input = fs.readFileSync('./2751.txt').toString().trim().split('\n').map(Number);
+// const fs = require("fs");
+// let input = fs.readFileSync('./2751.txt').toString().trim().split('\n').map(Number);
 
-input.shift()
-input.sort((a,b) => a - b)
+// input.shift()
+// input.sort((a,b) => a - b)
 
-console.log(...input)
+// console.log(...input)
 
 ////////
 
+const fs = require("fs");
+let input = fs.readFileSync('./2751.txt').toString().trim().split('\n').map(Number);
