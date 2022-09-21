b3dc99515cf9e4e8835d0324e346971e28e27df2
mj-song00 <niveus128@gmail.com>
2022-09-21 21:00:23 +0900

    2022.09.21

--- 2751.js
+++ 2751.js
@@ -6,7 +6,24 @@
 
 // console.log(...input)
 
-////////
+//////// 2번 풀이 다 틀렸따.. 띠로리 
 
 const fs = require("fs");
-let input = fs.readFileSync('./2751.txt').toString().trim().split('\n').map(Number);
+const input = fs.readFileSync('./2751.txt').toString().trim().split('\n').map(Number);
+ 
+input.sort((a,b) => a - b)
+
+// function getCount(input){
+//     return input.reduce((pv, cv) =>{
+//         pv[cv] = (pv[cv] || 0) + 1;
+//         return pv;
+//     }, {})
+// }
+
+
+
+// console.log(getCount(input))
+
+const set = new Set(input)
+const newArr = set
+console.log(...newArr)
